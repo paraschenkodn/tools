be99be8b8226469b01424242651d331fcd5ed56e
paraschenkodn <paraschenkodn@yandex.ru>
2015-11-12 17:58:37 +0600

    121115 1758

--- mapbuilder.h
+++ mapbuilder.h
@@ -18,8 +18,7 @@ public:
     void sphereMap();   // строим сферообразную карту
     void newmap();      // готовим новые данные для карт
 
-    void addPairHostsSt(QString &, QString &);    // добавление пары хостов (передатчик-приёмник)
-    float gridStep;     // шаг сетки координат
+    void addPairHostsSt(QString &tr, QString &rec);    // добавление пары хостов (передатчик-приёмник)
     void parserTRT();   // парсер данных команды tracert
 
     int currentmap;        // текущий вид карты
@@ -29,6 +28,18 @@ public:
     QVector<float>    vertices;         // пары координат (передатчик-приёмник)
     QVector<QString>    caption;    // надписи для точек
 
+    // ********* ПЛОСКАЯ КАРТА ********** функции и переменные
+    QVector< QVector<QString> > fmap;   // координатная сетка точек (индексов точек пар) служит для построения
+    float gridStep;     // шаг сетки координат
+    int sizefmap;   // размер координатной сетки
+    void resizemap()
+    {
+        fmap.resize(sizefmap);
+        for (int i=0;i<sizefmap;i++) {
+            fmap[i].resize(sizefmap);
+         }
+    }   // только в сторону увеличения
+    bool getpointfmap(QString &data, int &row, int &col);     // найти заданную точку по данным  (дата row col), возвращает row col, bool - true if found
 };
 
 //#include "buildermap.h"
