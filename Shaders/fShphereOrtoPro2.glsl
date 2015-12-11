4fbc8e4d2a2ce90258cda3eb0393dcc609a2563a
paraschenkodn <paraschenkodn@yandex.ru>
2015-12-11 16:52:50 +0600

    111215 1652

--- Shaders/fShphereOrtoPro2.glsl
+++ Shaders/fShphereOrtoPro2.glsl
@@ -12,13 +12,13 @@ void main(void) {
     ndcPos.xy = ((gl_FragCoord.xy / viewport.zw)*2.0) - 1.0;
     ndcPos.z = (2.0 * gl_FragCoord.z - gl_DepthRange.near - gl_DepthRange.far) / (gl_DepthRange.far - gl_DepthRange.near);
     ndcPos.w = 1.0;
-    vec4 ndc_current_pixel = ndcPos / gl_FragCoord.w  ;  //приводим к экранным координатам
+    vec4 current_pixel = ndcPos / gl_FragCoord.w  ;  //приводим к экранным координатам
 
-    vec2 diff = ndc_current_pixel.xy - center.xy;  // вычисляем разницу векторов между точкой и центром
+    vec2 diff = current_pixel.xy - center.xy;  // вычисляем разницу векторов между точкой и центром
 
     vec3 current_center = center;
 
-    ndc_current_pixel = gl_FragCoord;
+    current_pixel = gl_FragCoord;
             ndcPos.x = position.x / position.w;
             ndcPos.y = position.y / position.w;
             ndcPos.z = position.z / position.w;
@@ -27,19 +27,19 @@ void main(void) {
             current_center.z = gl_DepthRange.diff/2.0 * ndcPos.z + (gl_DepthRange.near + gl_DepthRange.far)/2;
     diff = gl_FragCoord.xy - current_center.xy; //*/
 
-    float d2 = dot(diff,diff);              // ??скалярное произведение разницы на разницу??
+    float d2 = dot(diff,diff);              // скалярное произведение вектора разницы на себя даёт размер умноженного вектора
     float r2 = radius*radius;               // радиус на радиус, теперь можем сравнить два скалярных произведения
 
     //gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
     if (d2>r2) {        // если скаляр радиуса меньше скаляра вектора от центра сферы до точки поверхности, отбрасываем, иначе рисуем сферу
         discard;
     } else {
-        vec3 l = normalize(gl_LightSource[0].position.xyz);
-        float dr =  sqrt(r2-d2);
-        vec3 n = vec3((ndc_current_pixel.xy - current_center.xy), dr);
-        float intensity = .2 + max(dot(l,normalize(n)), 0.0);
+        vec3 l = normalize(gl_LightSource[0].position.xyz); // нормализованный вектор направления на позицию источника света
+        float dr =  sqrt(r2-d2);    // размер вектора по оси Z
+        vec3 n = vec3((current_pixel.xy - current_center.xy), dr);  // направление вектора нормали проходящей через текущий пиксель
+        float intensity = .2 + max(dot(l,normalize(n)), 0.0);   // как только достигается перпендикулярность и далее dot<0.0, "так наступает тёмная сторона силы" 0.2
         gl_FragColor = gl_Color*intensity;
-        gl_FragDepth =  gl_FragCoord.z;// - dr*gl_DepthRange.diff/2.0*gl_ProjectionMatrix[2].z;
+        gl_FragDepth =  gl_FragCoord.z - dr;// - dr*gl_DepthRange.diff/2.0*gl_ProjectionMatrix[2].z;
         //gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
     }
     //gl_FragColor = gl_Color;
